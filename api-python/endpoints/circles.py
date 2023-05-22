01df1a6cf3fc1c77318c593719c82d4d2fdb7396
Adrien <adrien.lorge@epita.fr>
2023-05-22 19:05:30 +0200

    Endpoints marchent

--- api-python/endpoints/circles.py
+++ api-python/endpoints/circles.py
@@ -3,7 +3,22 @@ from datetime import datetime
 import psycopg2
 
 def circles_endpoints(app, r, conn):
-    # Route pour obtenir toutes les lettres
+
+    def getOrCreateWritterId(username):
+        cursor = conn.cursor()
+        cursor.execute('SELECT * FROM writer WHERE name = %s;', (username,))
+        writter = cursor.fetchone()
+        if writter:
+            return writter[0]
+        else:
+            cursor.execute('INSERT INTO writer (name,pseudo) VALUES (%s,%s) RETURNING id;', (username,username))
+            writter_id = cursor.fetchone()[0]
+            conn.commit()
+            cursor.close()
+            return writter_id
+        
+    
+    # Route pour obtenir toutes les lettres d'un cercle
     @app.route('/circles', methods=['GET'])
     def get_circles():
         #get the username with keycloak (Oidc-Claim-Preferred-Username)
@@ -16,33 +31,18 @@ def circles_endpoints(app, r, conn):
         cursor.close()
         return jsonify(circles)
     
-    # Route pour mettre à jour une lettre par ID
-    @app.route('/circles/<int:id>', methods=['POST'])
-    def update_circle(id):
+    # Create a new circle
+    @app.route('/circles', methods=['POST'])
+    def create_circle():
         cursor = conn.cursor()
 
-        # Récupération des données envoyées dans le corps de la requête
-        circleid = id
-        writerid = 1  # Remplacez par la valeur souhaitée pour writerid
-        postAt = datetime.now()
-        content = request.json['content']
-        subject = request.json['subject']
-
-        # Vérification des champs obligatoires
-        if circleid is None or content is None or subject is None:
-                return jsonify({'error': 'circleid, content, and subject are required fields'}), 400
-
+        name = request.json['name']
         try:
-            # Exécution de la requête SQL pour insérer la nouvelle lettre
             cursor.execute(
-                'INSERT INTO circle (circleId, writerId, postAt, content, subject) VALUES (%s, %s, %s, %s, %s) RETURNING id;',
-                (circleid, writerid, postAt, content, subject)
+                'INSERT INTO circle (name) VALUES (%s) RETURNING id;',
+                (name,)
             )
-
-            # Récupération de l'ID généré pour la nouvelle lettre
             circle_id = cursor.fetchone()[0]
-
-            # Validation de la transaction et fermeture du curseur
             conn.commit()
             cursor.close()
 
@@ -51,4 +51,59 @@ def circles_endpoints(app, r, conn):
             conn.rollback()
             cursor.close()
             return jsonify({'error': f'Failed to create circle: {e}'}), 500
+    
+    # Delete circle with id
+
+    # @app.route('/circles/<int:id>', methods=['DELETE'])
+    # def delete_circle(id):
+    #     cursor = conn.cursor()
+
+    #     try:
+    #         cursor.execute('DELETE FROM circle WHERE id = %s;', (id,))
+    #         cursor.execute('DELETE FROM \"writerCircle\" WHERE circleId = %s;', (id,))
+    #         cursor.execute('DELETE FROM letter WHERE circleId = %s;', (id,))
+
+    #         # Manque l'update du champ reply pour toutes les lettres qui référence des lettres supprimés
+            
+    #         conn.commit()
+    #         cursor.close()
+
+    #         return jsonify({'message': f'circle deleted successfully with ID {id}'}), 201
+    #     except psycopg2.Error as e:
+    #         conn.rollback()
+    #         cursor.close()
+    #         return jsonify({'error': f'Failed to delete circle: {e}'}), 500
+    
+    # Join a new circle
+    @app.route('/circles/<int:id>/join', methods=['POST'])
+    def join_circle(id):
+        cursor = conn.cursor()
+        username = request.headers.get('X-Remote-User')
+        try:
+            userId = getOrCreateWritterId(username)
+            cursor.execute('INSERT INTO \"writerCircle\" VALUES (%s, %s);', (id, userId,))
+            conn.commit()
+            cursor.close()
+            return jsonify({'sucess': f'Sucess in joining circle with id {id}'})
+        except psycopg2.Error as e:
+            conn.rollback()
+            cursor.close()
+            return jsonify({'error': f'Failed to join circle: {e}; id = {id}, userId = {userId}'}), 500
+        
+    # Quit a circle
+    @app.route('/circles/<int:id>/quit', methods=['PATCH'])
+    def quit_circle(id):
+        cursor = conn.cursor()
+        username = request.headers.get('X-Remote-User')
+        try:
+            userId = getOrCreateWritterId(username)
+            cursor.execute('DELETE FROM \"writerCircle\" WHERE \"circleId\" = %s AND \"writerId\" = %s;', (id, userId,))
+            conn.commit()
+            cursor.close()
+            return jsonify({'sucess': f'Sucess in quiting circle with id {id}'})
+        except psycopg2.Error as e:
+            conn.rollback()
+            cursor.close()
+            return jsonify({'error': f'Failed to quit circle: {e}'}), 500
+
         
--- api-python/endpoints/writer.py
+++ api-python/endpoints/writer.py
@@ -4,52 +4,27 @@ import psycopg2
 
 def writer_endpoints(app, r, conn):
 
-    def getWriterID(username):
+    def getOrCreateWritterId(username):
         cursor = conn.cursor()
-        try:
-            cursor.execute('SELECT * FROM writer WHERE pseudo = %s;', (username,))
-            writter = cursor.fetchone()
+        cursor.execute('SELECT * FROM writer WHERE name = %s;', (username,))
+        writter = cursor.fetchone()
+        if writter:
+            return writter[0]
+        else:
+            cursor.execute('INSERT INTO writer (name,pseudo) VALUES (%s,%s) RETURNING id;', (username,username))
+            writter_id = cursor.fetchone()[0]
+            conn.commit()
             cursor.close()
-            if writter:
-                return writter[0]
-            else:
-                return None
-        except psycopg2.Error as _:
-            return 
-        
-    # username = request.headers.get('X-Remote-User')
-    # id = getOrCreateWritterId(username)
-        
-    # Endpoints functions
+            return writter_id
+    
     @app.route('/writer', methods=['GET'])
-    def get_writers():
+    def get_writer():
         cursor = conn.cursor()
-        try:
-            cursor.execute('SELECT * FROM writer;')
-            writer = cursor.fetchall()
-            cursor.close()
-            return jsonify(writer), 200
-        except psycopg2.Error as e:
-            cursor.close()
-            return jsonify({'error': f'Failed to get writers: {e}'}), 500
-    
-    
-    @app.route('/writer/<int:id>', methods=['GET'])
-    def get_writer_by_id(id):
-        cursor = conn.cursor()
-        try:
-            cursor.execute('SELECT * FROM writer WHERE id = %s;', (id,))
-            writer = cursor.fetchone()
-            cursor.close()
-            if writer:
-                return jsonify(writer), 200
-            else:
-                return jsonify({'error': 'writer not found'}), 404
-        except psycopg2.Error as e:
-            cursor.close()
-            return jsonify({'error': f'Failed to get writer: {e}'}), 500
-            
-    
+        cursor.execute('SELECT * FROM writer;')
+        circles = cursor.fetchall()
+        cursor.close()
+        return jsonify(circles)
+
     @app.route('/writer', methods=['POST'])
     def post_writer():
         cursor = conn.cursor()
@@ -68,29 +43,25 @@ def writer_endpoints(app, r, conn):
             cursor.execute('INSERT INTO writer (title, name, pseudo) VALUES (%s,%s,%s) RETURNING id;', (title, name, pseudo,))
             writter_id = cursor.fetchone()[0]
             cursor.close()
-            return jsonify({"json", f"ify {str(writter_id)}"}), 200
+            return jsonify({'sucess': f"id = {writter_id}"})
         except psycopg2.Error as e:
             cursor.close()
-            return jsonify({'error', f'Failed to post new writer: {e}'}), 500
+            return jsonify({'error': f'Failed to post new writer: {e}'}), 500
     
-    @app.route('/writer', methods=['PUT'])
+    @app.route('/writer', methods=['PATCH'])
     def put_writer():
         cursor = conn.cursor()
-        username = "Adrien-Elek"
-        # username = request.headers.get('X-Remote-User')
+        username = request.headers.get('X-Remote-User')
+        id = getOrCreateWritterId(username)
         try:
-            cursor.execute('SELECT * FROM writer WHERE pseudo = %s;', (username,))
+            cursor.execute('SELECT * FROM writer WHERE id = %s;', (id,))
             writer = cursor.fetchone()
-            if not writer:
-                cursor.close()
-                return jsonify({'error': f'Invalid User {username}'}), 200
             
             body = request.get_json()
             title = body["title"] if body["title"] != None else writer[1]
-            name = body["name"] if body["name"] != None else writer[2]
-            pseudo = body["pseudo"] if body["pseudo"] != None else writer[3]
+            pseudo = body["pseudo"] if body["pseudo"] != None else writer[2]
 
-            cursor.execute('UPDATE writer SET title = %s, name = %s, pseudo = %s where id = %s;', (title, name, pseudo, writer[0],))
+            cursor.execute('UPDATE writer SET title = %s, pseudo = %s where id = %s;', (title, pseudo, writer[0],))
             cursor.close()
             return jsonify({'success': f'Update writer {writer[0]}'}), 200
 
@@ -98,23 +69,23 @@ def writer_endpoints(app, r, conn):
             cursor.close()
             return jsonify({'error': f'Failed to post new writer: {e}'}), 500
     
-    @app.route('/writer/<int:id>', methods=['DELETE'])
-    def delete_writer(id):
-        cursor = conn.cursor()
-        # username = request.headers.get('X-Remote-User')
-        try:
-            cursor.execute('SELECT * FROM writer WHERE id = %s;', (id,))
-            writer = cursor.fetchone()
-            if not writer:
-                cursor.close()
-                return jsonify({'error': f'Invalid User with id: {id}'}), 200
+    # @app.route('/writer/<int:id>', methods=['DELETE'])
+    # def delete_writer(id):
+    #     cursor = conn.cursor()
+    #     # username = request.headers.get('X-Remote-User')
+    #     try:
+    #         cursor.execute('SELECT * FROM writer WHERE id = %s;', (id,))
+    #         writer = cursor.fetchone()
+    #         if not writer:
+    #             cursor.close()
+    #             return jsonify({'error': f'Invalid User with id: {id}'}), 200
 
-            cursor.execute('DELETE FROM writer WHERE id = %s;', (id,))
-            cursor.execute('DELETE FROM writerCircle WHERE writerId = %s;', (id,))
+    #         cursor.execute('DELETE FROM writer WHERE id = %s;', (id,))
+    #         cursor.execute('DELETE FROM writerCircle WHERE writerId = %s;', (id,))
 
-            cursor.close()
-            return jsonify({'success': f'Deleted writer {writer[0]}'}), 200
+    #         cursor.close()
+    #         return jsonify({'success': f'Deleted writer {writer[0]}'}), 200
 
-        except psycopg2.Error as e:
-            cursor.close()
-            return jsonify({'error': f'Failed to delete writer: {e}'}), 500
+    #     except psycopg2.Error as e:
+    #         cursor.close()
+    #         return jsonify({'error': f'Failed to delete writer: {e}'}), 500
